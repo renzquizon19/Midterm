2f1845d54b77b786a98af948c7b63968d45c28c0
renzquizon19 <ram.renz19@gmail.com>
2022-11-25 13:23:21 +0800

    Successfully added datasets

---
+++ Shopping_Cart/datasets.php
@@ -0,0 +1,60 @@
+<?php
+    $arrProducts = array(
+        array(
+            'name'          => "Adidas",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 3,300.00",
+            'photo1'        => "adidas1.jpg",
+            'photo2'        => "adidas2.jpg",
+        ),
+        array(
+            'name'          => "Adidas",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 3,600.00",
+            'photo1'        => "adidas3.jpg",
+            'photo2'        => "adidas4.jpg",
+        ),
+        array(
+            'name'          => "Adidas",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 5,500.00",
+            'photo1'        => "adidas5.jpg",
+            'photo2'        => "adidas6.jpg",
+        ),
+        array(
+            'name'          => "Adidas",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 4,000.00",
+            'photo1'        => "adidas7.jpg",
+            'photo2'        => "adidas8.jpg",
+        ),
+        array(
+            'name'          => "Nike",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 3,295.00",
+            'photo1'        => "nike2.jpg",
+            'photo2'        => "nike1.jpg",
+        ),
+        array(
+            'name'          => "Nike",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 4,195.00",
+            'photo1'        => "nike3.jpg",
+            'photo2'        => "nike4.jpg",
+        ),
+        array(
+            'name'          => "Nike",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 3,295.00",
+            'photo1'        => "nike5.jpg",
+            'photo2'        => "nike6.jpg",
+        ),
+        array(
+            'name'          => "Nike",
+            'description'   => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo impedit commodi nostrum, incidunt dignissimos repellendus, hic laboriosam repudiandae alias fugiat, suscipit est ea natus? At fuga quis consequatur, dolor tenetur architecto atque laborum, tempora velit expedita odit sint, minima et. Aperiam quas corporis officiis aspernatur vero sunt aliquam eos.",
+            'price'         => "₱ 3,295.00",
+            'photo1'        => "nike7.jpg",
+            'photo2'        => "nike8.jpg",
+        )
+    );
+?>
